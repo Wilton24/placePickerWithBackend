7f092c57ade9b8360c7f5bdd56e49fb68bdcb996
Wilton Batiquin <cbasher24@gmail.com>
2025-07-18 23:42:31 +0800

    add axios dependency and implement data fetching in AvailablePlaces component

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,31 @@
 import Places from './Places.jsx';
+import { useState } from 'react';
+import axios from 'axios';
+
+
+const places = localStorage.getItem('places');
+
+const sendData = async () => {
+  try {
+    const response = await axios.get("http://localhost:3000/places");
+    const data = response.data.places;
+    localStorage.setItem('places', JSON.stringify(data));
+
+  } catch (error) {
+    console.log('Error fetching places:', error);
+  }
+};
+
+sendData();
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
