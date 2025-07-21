517e14f78f3473b85dd316879b24be61a6ecb91c
Wilton Batiquin <cbasher24@gmail.com>
2025-07-21 23:39:16 +0800

    fix: correct API endpoint and ensure places are sorted by distance based on user location

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,7 +2,7 @@ import Places from './Places.jsx';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import ErrorPage from './ErrorPage.jsx';
-
+import { sortPlacesByDistance } from "../loc.js"
 // const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
@@ -14,7 +14,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
     const fetchData = async () => {
       setIsFetching(true);
       try {
-        const response = await axios.get("http://localhost:3000/placeswd");
+        const response = await axios.get("http://localhost:3000/places");
         const data = response.data.places;
         // if (!response.ok) {
         //   throw new Error('Failed to fetch places');
@@ -22,12 +22,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
         if (response.request.statusText !== "OK") {
           throw new Error('Failed to fetch places');
         }
-
-        setAvailablePlaces(data);
+        navigator.geolocation.getCurrentPosition((position) => {
+          // console.log(position.coords.latitude, position.coords.longitude);
+          const sortedPlaces = sortPlacesByDistance(data, position.coords.latitude, position.coords.longitude);
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({ message: error.message || "An error occurred while fetching places" });
+        setIsFetching(false);
       }
-      setIsFetching(false);
     };
 
     fetchData();
